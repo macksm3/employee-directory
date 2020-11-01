43c784dc3342a98a52b7d7732488a682946a1805
MacksM3 <64023042+macksm3@users.noreply.github.com>
2020-11-01 17:12:53 -0500

    display image working

--- src/components/DisplayTable/index.js
+++ src/components/DisplayTable/index.js
@@ -1,16 +1,69 @@
 import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import BTable from 'react-bootstrap/Table';
 // import styled from 'styled-components'
-import { useTable, useSortBy } from 'react-table'
+import { useTable, useFilters, useGlobalFilter, useSortBy } from 'react-table'
+import {matchSorter} from 'match-sorter'
 import TestList from "./TestList";
 import "./style.css";
 
+// Define a default UI for filtering
+function DefaultColumnFilter({
+  column: { filterValue, preFilteredRows, setFilter },
+}) {
+  const count = preFilteredRows.length
 
+  return (
+    <input
+      value={filterValue || ''}
+      onChange={e => {
+        setFilter(e.target.value || undefined) // Set undefined to remove the filter entirely
+      }}
+      placeholder={`Search ${count} records...`}
+    />
+  )
+}
 
+function fuzzyTextFilterFn(rows, id, filterValue) {
+  return matchSorter(rows, filterValue, { keys: [row => row.values[id]] })
+}
+
+// Let the table remove the filter if the string is empty
+fuzzyTextFilterFn.autoRemove = val => !val
+
+// Our table component
 function Table({ columns, data }) {
+  const filterTypes = React.useMemo(
+    () => ({
+      // Add a new fuzzyTextFilterFn filter type.
+      fuzzyText: fuzzyTextFilterFn,
+      // Or, override the default text filter to use
+      // "startWith"
+      text: (rows, id, filterValue) => {
+        return rows.filter(row => {
+          const rowValue = row.values[id]
+          return rowValue !== undefined
+            ? String(rowValue)
+                .toLowerCase()
+                .startsWith(String(filterValue).toLowerCase())
+            : true
+        })
+      },
+    }),
+    []
+  )
+
+  const defaultColumn = React.useMemo(
+    () => ({
+      // Let's set up our default Filter UI
+      Filter: DefaultColumnFilter,
+    }),
+    []
+  )
+
   // Use the state and functions returned from useTable to build your UI
   const {
     getTableProps,
-    getTableBodyProps,
     headerGroups,
     rows,
     prepareRow,
@@ -18,7 +71,11 @@ function Table({ columns, data }) {
     {
       columns,
       data,
+      defaultColumn,
+      filterTypes,
     },
+    useFilters,
+    useGlobalFilter,
     useSortBy
   )
 
@@ -27,7 +84,7 @@ function Table({ columns, data }) {
 
   return (
     <>
-      <table {...getTableProps()}>
+      <BTable striped bordered hover size="sm" {...getTableProps()}>
         <thead>
           {headerGroups.map(headerGroup => (
             <tr {...headerGroup.getHeaderGroupProps()}>
@@ -44,12 +101,13 @@ function Table({ columns, data }) {
                         : ' 🔼'
                       : ''}
                   </span>
+                  <div>{column.canFilter ? column.render('Filter') : null}</div>
                 </th>
               ))}
             </tr>
           ))}
         </thead>
-        <tbody {...getTableBodyProps()}>
+        <tbody>
           {firstPageRows.map(
             (row, i) => {
               prepareRow(row)
@@ -64,7 +122,7 @@ function Table({ columns, data }) {
               )}
           )}
         </tbody>
-      </table>
+      </BTable>
     </>
   )
 }
@@ -75,10 +133,15 @@ function DisplayTable() {
       {
         Header: 'Image',
         accessor: 'image',
+        Cell: ({ cell: { value } }) => (<img src={value} alt=""/>),
+        disableSortBy: true,
+        disableFilters: true,
       },
       {
         Header: 'Name',
         accessor: 'name',
+        // Use our custom `fuzzyText` filter on this column
+        filter: 'fuzzyText',
       },
       {
         Header: 'Phone',
